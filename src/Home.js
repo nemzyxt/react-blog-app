59e3810a1896464f8a762e91c8061ed1935a1986
Nemuel Wainaina <bR0S3@protonmail.com>
2023-06-09 08:15:57 +0300

    Click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log("Hello champ !")
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("Hello " + name)
+    }
+
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => {
+                handleClickAgain("Nemuel")
+            }}>Click me again</button>
         </div>
     );
 }
