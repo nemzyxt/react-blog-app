6a3cad8e87eca158c8d5daa8425c4abd0eb6f8a4
Nemuel Wainaina <bR0S3@protonmail.com>
2023-06-09 07:58:46 +0300

    Import to root component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const link = "http://www.google.com"
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        <p>{ 10 }</p>
-        <p>{ "Hello friend" }</p>
-        <p>{ [1, 2, 3, 4, 5] }</p>
-        <p>{ Math.random() * 10 }</p>
-        <a href={ link }>Google site</a>
+        
       </div>
     </div>
   );
